e28096e1a57234ef316f3d565e852e2117e392fb
petak5 <peterurgos12345@gmail.com>
2024-04-04 22:58:57 +0300

    Video 04

--- RPG Project/Assets/Scripts/Interactable.cs
+++ RPG Project/Assets/Scripts/Interactable.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-/*	
+/*
 	This component is for all objects that the player can
 	interact with such as enemies, items etc. It is meant
 	to be used as a base class.
@@ -58,6 +58,11 @@ public class Interactable : MonoBehaviour {
 	// Draw our radius in the editor
 	void OnDrawGizmosSelected ()
 	{
+		if (interactionTransform == null)
+		{
+			interactionTransform = transform;
+		}
+
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawWireSphere(interactionTransform.position, radius);
 	}
